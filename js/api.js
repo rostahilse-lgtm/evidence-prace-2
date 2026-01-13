45ec7613d40750e031122493aa7a61a9b8f44be1
rostahilse-lgtm <rostahilse@gmail.com>
2026-01-13 13:08:57 +0100

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,18 @@
-
+async function apiCall(action, params = {}) {
+  const apiUrl = localStorage.getItem('apiUrl') || DEFAULT_API_URL;
+  const url = new URL(apiUrl);
+  url.searchParams.append('action', action);
+  Object.keys(params).forEach(key => {
+    if (params[key] !== null && params[key] !== undefined) {
+      url.searchParams.append(key, params[key]);
+    }
+  });
+  
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return { code: '999', error: 'Chyba připojení: ' + error.message };
+  }
+}
